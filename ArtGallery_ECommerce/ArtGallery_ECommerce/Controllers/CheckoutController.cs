ed50ca07ccf3dc751c32bf17fc06d84234b551ad
Steve White <stephenkrohnwhite@gmail.com>
2018-08-04 13:41:09 -0500

    updated order display, added order for testing with proper checkout controller action

--- ArtGallery_ECommerce/ArtGallery_ECommerce/Controllers/CheckoutController.cs
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/Controllers/CheckoutController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -54,8 +55,8 @@ namespace ArtGallery_ECommerce.Controllers
                 }
                 db.Order.Add(order);
                 db.SaveChanges();
-                
-                
+                order.PublicOrderNum = GetRandomizedString(order.OrderId);
+                db.SaveChanges();
             }
             catch
             {
@@ -107,6 +108,61 @@ namespace ArtGallery_ECommerce.Controllers
             return order;
             
         }
+        static String GetRandomizedString(Int32 input)
+        {
+            Int32 uniqueLength = 6;
+            Int32 randomLength = 4;
+            String uniqueString;
+            String randomString;
+            StringBuilder resultString = new StringBuilder(uniqueLength + randomLength);
 
+            Random randomizer = new Random(
+                    (Int32)(
+                        DateTime.Now.Ticks + (DateTime.Now.Ticks > input ? DateTime.Now.Ticks / (input + 1) : input / DateTime.Now.Ticks)
+                    )
+                );
+
+            randomString = EncodeInt32AsString(randomizer.Next(1, Int32.MaxValue), randomLength);
+            uniqueString = EncodeInt32AsString(input, uniqueLength);
+
+            resultString.AppendFormat("{0}\t {1}\t ", uniqueString, randomString);
+
+            for (Int32 i = 0; i < Math.Min(uniqueLength, randomLength); i++)
+            {
+                resultString.AppendFormat("{0}{1}", uniqueString[i], randomString[i]);
+            }
+            resultString.Append((uniqueLength < randomLength ? randomString : uniqueString).Substring(Math.Min(uniqueLength, randomLength)));
+
+            return resultString.ToString();
+        }
+
+        static String EncodeInt32AsString(Int32 input, Int32 maxLength = 0)
+        {
+            Char[] allowedList = new Char[] {
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+            'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+            'U', 'V', 'W', 'X', 'Y', 'Z' };
+            Int32 allowedSize = allowedList.Length;
+            StringBuilder result = new StringBuilder(input.ToString().Length);
+
+            Int32 moduloResult;
+            while (input > 0)
+            {
+                moduloResult = input % allowedSize;
+                input /= allowedSize;
+                result.Insert(0, allowedList[moduloResult]);
+            }
+
+            if (maxLength > result.Length)
+            {
+                result.Insert(0, new String(allowedList[0], maxLength - result.Length));
+            }
+
+            if (maxLength > 0)
+                return result.ToString().Substring(0, maxLength);
+            else
+                return result.ToString();
+        }
     }
 }
