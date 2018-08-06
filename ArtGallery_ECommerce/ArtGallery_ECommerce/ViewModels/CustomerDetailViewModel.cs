9a79551790127455fd9fcfe39f975eea873f83cc
Anina Reinhart <reinharta@outlook.com>
2018-08-06 00:19:43 -0500

    Fixed Customer and Employee Detail Actions/Views

---
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/ViewModels/CustomerDetailViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ArtGallery_ECommerce.ViewModels
+{
+    public class CustomerDetailViewModel
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+
+        [Display(Name ="City, State & Zipcode")]
+        public string CityStateZip { get; set; }
+
+
+        [Display(Name = "Phone")]
+        public string PhoneNumber { get; set; }
+    }
+}
