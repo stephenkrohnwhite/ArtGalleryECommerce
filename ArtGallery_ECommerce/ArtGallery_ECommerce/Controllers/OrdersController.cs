cee13b847679ac7af37ac6f9e335e6952e5dd995
Steve White <stephenkrohnwhite@gmail.com>
2018-08-07 18:06:35 -0500

    adjust order controller picklist

--- ArtGallery_ECommerce/ArtGallery_ECommerce/Controllers/OrdersController.cs
+++ ArtGallery_ECommerce/ArtGallery_ECommerce/Controllers/OrdersController.cs
@@ -43,7 +43,7 @@ namespace ArtGallery_ECommerce.Controllers
 
         public ActionResult PickList()
         {
-            var pickList = db.Order.Where(c => c.OrderStatus.Name == "Processing").Include(o => o.OrderStatus).ToList();
+            var pickList = db.Order.Where(c => c.OrderStatus.Name == "Processing").Include(o => o.OrderStatus).Include(b => b.Buyer).ToList();
             return View("PickList", pickList);
         }
 
@@ -76,83 +76,6 @@ namespace ArtGallery_ECommerce.Controllers
             return View();
         }
 
-        // POST: Orders/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "ProductId,CustomerOrderId,Quantity,TrackingId,OrderTime,StatusId,CustomerId")] Order order)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Order.Add(order);
-        //        db.SaveChanges();
-        //        order.PublicOrderNum = GetRandomizedString(order.OrderId);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.CustomerId = new SelectList(db.Customer, "CustomerId", "FirstName", order.CustomerId);
-        //    ViewBag.StatusId = new SelectList(db.Status, "StatusId", "Name", order.StatusId);
-        //    return View(order);
-        //}
-
-        //static String GetRandomizedString(Int32 input)
-        //{
-        //    Int32 uniqueLength = 6; 
-        //    Int32 randomLength = 4;
-        //    String uniqueString;
-        //    String randomString;
-        //    StringBuilder resultString = new StringBuilder(uniqueLength + randomLength);
-
-        //    Random randomizer = new Random(
-        //            (Int32)(
-        //                DateTime.Now.Ticks + (DateTime.Now.Ticks > input ? DateTime.Now.Ticks / (input + 1) : input / DateTime.Now.Ticks)
-        //            )
-        //        );
-
-        //    randomString = EncodeInt32AsString(randomizer.Next(1, Int32.MaxValue), randomLength);           
-        //    uniqueString = EncodeInt32AsString(input, uniqueLength);
- 
-        //    resultString.AppendFormat("{0}\t {1}\t ", uniqueString, randomString);
-
-        //    for (Int32 i = 0; i < Math.Min(uniqueLength, randomLength); i++)
-        //    {
-        //        resultString.AppendFormat("{0}{1}", uniqueString[i], randomString[i]);
-        //    }
-        //    resultString.Append((uniqueLength < randomLength ? randomString : uniqueString).Substring(Math.Min(uniqueLength, randomLength)));
-
-        //    return resultString.ToString();
-        //}
-
-        //static String EncodeInt32AsString(Int32 input, Int32 maxLength = 0)
-        //{ 
-        //    Char[] allowedList = new Char[] {
-        //    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-        //    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-        //    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-        //    'U', 'V', 'W', 'X', 'Y', 'Z' };
-        //    Int32 allowedSize = allowedList.Length;
-        //    StringBuilder result = new StringBuilder(input.ToString().Length);
-
-        //    Int32 moduloResult;
-        //    while (input > 0)
-        //    {
-        //        moduloResult = input % allowedSize;
-        //        input /= allowedSize;
-        //        result.Insert(0, allowedList[moduloResult]);
-        //    }
-
-        //    if (maxLength > result.Length)
-        //    {
-        //        result.Insert(0, new String(allowedList[0], maxLength - result.Length));
-        //    }
-
-        //    if (maxLength > 0)
-        //        return result.ToString().Substring(0, maxLength);
-        //    else
-        //        return result.ToString();
-        //}
 
         // GET: Orders/Edit/5
         public ActionResult Edit(int? id)
